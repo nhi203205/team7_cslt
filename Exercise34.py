0cfdc4370d283ca73e0d38ec2eca8edb6b0b5537
Nguyen Thi Thao Nhi <146497353+nhi203205@users.noreply.github.com>
2023-11-11 08:52:02 +0700

    BaiTap_if

---
+++ Exercise34.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a number: "))
+if n == 0:
+    print("Not an even number, not an odd number")
+elif n % 2 == 0:
+    print(n,"is an even number")
+else:
+    print(n,"is an odd number" )
